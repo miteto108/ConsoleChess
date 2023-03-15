266f7043ac27287a1ac1d627f0abe1e4a27a3695
miteto108 <72306873+miteto108@users.noreply.github.com>
2023-03-15 10:57:25 +0200

    Add files via upload

---
+++ FirstCommit-Skeleton/MyChess/BoardTesting.hpp
@@ -0,0 +1,22 @@
+
+#ifndef _BOARD_TESTS_
+#define _BOARD_TESTS_
+
+#include "Square.hpp"
+#include "Pieces.hpp"
+#include "Board.hpp"
+
+
+#include "../MyChessTesting/catch2.hpp"
+
+
+
+
+TEST_CASE("Board","[board]"){
+
+
+
+}
+
+
+#endif
---
+++ FirstCommit-Skeleton/MyChess/Main.cpp
@@ -0,0 +1,21 @@
+#include "Square.hpp"
+#include "Pieces.hpp"
+#include "Board.hpp"
+
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main(){
+
+    vector<string> g;
+    cout<<g.empty();
+    g.push_back("123");
+    cout<<g.empty();
+
+
+
+    return 0;
+}
