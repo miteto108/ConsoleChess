4ad70e47b5cdccbe68fccf9e11aaf2d5e3191340
miteto108 <72306873+miteto108@users.noreply.github.com>
2023-03-15 11:04:58 +0200

    Delete FirstCommit-Skeleton directory

--- FirstCommit-Skeleton/MyChessTesting/MainTesting.cpp
+++
@@ -1,8 +0,0 @@
-#define CATCH_COFIG_MAIN
-
-
-#include "../MyChess/SquareTesting.hpp"
-#include "../MyChess/PiecesTesting.hpp"
-#include "../MyChess/BoardTesting.hpp"
-#include "../MyChess/CommandInterfaceTesting.hpp"
-
