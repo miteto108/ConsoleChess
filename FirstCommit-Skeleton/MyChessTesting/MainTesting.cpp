42ad0d10bf31c0e7e799d2d9e32738a5bb8eb52d
miteto108 <72306873+miteto108@users.noreply.github.com>
2023-03-15 10:58:26 +0200

    Add files via upload

---
+++ FirstCommit-Skeleton/MyChessTesting/MainTesting.cpp
@@ -0,0 +1,8 @@
+#define CATCH_COFIG_MAIN
+
+
+#include "../MyChess/SquareTesting.hpp"
+#include "../MyChess/PiecesTesting.hpp"
+#include "../MyChess/BoardTesting.hpp"
+#include "../MyChess/CommandInterfaceTesting.hpp"
+
